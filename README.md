a20dacfb8ce521afb45f9bbe09a3d08715b79335
CrugBarat <craigtbarratt@gmail.com>
2020-06-18 22:32:52 +0100

    inital commit
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

---
+++ README.md
@@ -0,0 +1,32 @@
+<img src="https://github.com/CrugBarat/my_files/blob/master/step/step.png">
+
+# Step Sequencer
+
+An app that uses Tone.js to create a basic step sequencer.
+
+**Programming Language**: JavaScript
+
+**Library**: [Tone.js](https://tonejs.github.io/)
+
+**Markup/Styling**: HTML5/SCSS
+
+**Notes**: C3 -> C4
+
+**Oscillation**: Triangle
+
+---
+
+# Setup
+
+- Download the files
+
+- Install live-server:
+
+```
+npm install -g live-server
+```
+- Navigate to project folder and run:
+
+```
+live-server
+```
---
+++ main.js
@@ -0,0 +1,35 @@
+document.documentElement.addEventListener('mousedown', () => {
+  if (Tone.context.state !== 'running') Tone.context.resume();
+});
+
+const synths = [
+  new Tone.Synth(),
+  new Tone.Synth(),
+  new Tone.Synth(),
+  new Tone.Synth(),
+  new Tone.Synth(),
+  new Tone.Synth(),
+  new Tone.Synth(),
+  new Tone.Synth()
+];
+
+synths.forEach(synth => synth.toMaster());
+
+const rows = document.body.querySelectorAll('div > div');
+const notes = ['C4', 'B3', 'A3', 'G3', 'F3', 'E3', 'D3', 'C3'];
+let index = 0;
+
+function repeat(time) {
+  let step = index % 8;
+  for (let i = 0; i < rows.length; i++) {
+    let synth = synths[i],
+        note = notes[i],
+        row = rows[i],
+        input = row.querySelector(`input:nth-child(${step + 1})`);
+    if (input.checked) synth.triggerAttackRelease(note, '8n', time);
+  }
+  index++;
+}
+
+Tone.Transport.scheduleRepeat(repeat, '8n');
+Tone.Transport.start();
